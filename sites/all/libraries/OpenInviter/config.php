9a83ea1cbb8dadfc468c1636816952fbdbdbbdca
amankanoria <aman.kanoria@gmail.com>
2013-07-26 17:50:23 +0530

    bahut ho gya yar

---
+++ sites/all/libraries/OpenInviter/config.php
@@ -0,0 +1,5 @@
+<?php
+$openinviter_settings=array(
+'username'=>"amankanoria", 'private_key'=>"3d91cb1b168a44167d3cccdb36fe7326", 'cookie_path'=>"/tmp", 'message_body'=>"You are invited to tach.in", 'message_subject'=>" is inviting you to tach.in", 'transport'=>"curl", 'local_debug'=>"on_error", 'remote_debug'=>"", 'hosted'=>"", 'proxies'=>array(),
+'stats'=>"", 'plugins_cache_time'=>"1800", 'plugins_cache_file'=>"oi_plugins.php", 'update_files'=>"1", 'stats_user'=>"", 'stats_password'=>"");
+?>
